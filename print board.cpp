02cf909be38c2aaa5353e6037a2ef17528ef81ec
LelsQ <68802636+LelsQ@users.noreply.github.com>
2021-03-28 10:13:37 +0700

    Update print board.cpp

--- print board.cpp
+++ print board.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 bool keluar(int pilihan) {      //Fungsi keluar
@@ -11,64 +14,130 @@ bool keluar(int pilihan) {      //Fungsi keluar
     }
 }
 
-int gerak_x(string arah, int x) {      //Fungsi nentuin koordinat x selanjutnya
-    if (arah == "kanan") {
-        x = x + 1;
-        return x;
+bool range_check(int x, int y, int x_kecoa, int y_kecoa ) // fungsi pengecekan jarak guna penyerangan
+{
+    if (abs(x-x_kecoa) <= 2 && abs(y-y_kecoa) <=2){
+        return true;
     }
-    else if (arah == "kiri") {
-        x = x - 1;
-        return x;
-    }
-    else {
-        return x;
+    else{
+        return false;
     }
 }
 
-int gerak_y(string arah, int y) {       //Fungsi nentuin koordinat y selanjutnya
-    if (arah == "bawah") {
-        y = y + 1;
-        return y;
+void gerak_robot(string arah, int *x, int *y) {      //Fungsi nentuin koordinat selanjutnya
+    if (arah == "kanan") {
+        *x = *x + 1;
     }
-    else if (arah == "atas") {
-        y = y - 1;
-        return y;
+    else if (arah == "kiri") {
+        *x = *x - 1;
     }
-    else {
-        return y;
+    else if (arah == "bawah") {
+        *y = *y + 1;
+    }
+    else { // untuk arah atas
+        *y = *y - 1;
     }
 }
+
+bool checking_loct(string arah,int x, int y){ //buat check geraknya ke luar zona atau enggak
+    if (arah == "kanan") {
+        if((x+1)<=9){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else if (arah == "kiri") {
+        if((x-1)>=0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else if (arah == "bawah") {
+        if((y+1)<=9){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else { // untuk arah atas
+        if ((y-1)>=0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+
 //Buat fungsi cetak board yang nerima parameter x,y
 
 // Fungsi cek input arah, pilihan
 //Cek bakal keluar peta ato nabrak kecoak ato ngga
 
 int main() {
+    // Kamus
+    int x, y,x_kecoa,y_kecoa,pilihan;
+    bool flag = false;
+    char board[10][10];
+    srand((unsigned)time(0));
+
+    // initiate
+    x_kecoa=(rand()%9)+1; //random x 1-10
+    y_kecoa=(rand()%9)+1; //random x 1-10
+    x = 0;
+    y = 0;
+
+    cout << "=========== ROBOT PEMBASMI KECOA ===========" << endl;
+    cout << "============================================" << endl << endl;
+    cout << "============== TAMPILAN BOARD ==============" << endl << endl;
+    for (int i=0;i<10;i++){
+        for (int j=0;j<10;j++){
+            if ((i==y) && (j==x)){
+                board[i][j] = 'R';
+            }
+            else if ((i==y_kecoa)&&(j==x_kecoa)){
+                board[i][j] = 'K';
+            }
+            else{
+                board[i][j] = '.';
+            }
+            cout << board[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
     cout << "1. Bergerak \n" << "3. Keluar \n";
 
-    int pilihan;
     cout << "Masukkan pilihan: ";
     cin >> pilihan;
 
-    int x, y;
-    x = 1;
-    y = 1;
-    bool flag = false;
+
 
     while (flag == false) {
-        
+
         if (pilihan == 1) {
             string arah;
             cout << "Masukkan arah gerak (kanan,kiri,atas,bawah) : \n";
             cin >> arah;
-            x = gerak_x(arah, x);
-            y = gerak_y(arah, y);
-            
-            char board[10][10];        
-            for (int i = 1; i < 11; i++) {
-                for (int j = 1; j < 11; j++) {
+            cout << endl;
+            if (checking_loct(arah,x,y)){
+                gerak_robot(arah, &x,&y); // fungsi update posisi dari robot
+            }
+            else{
+                cout << "PERHATIAN!! ROBOT TIDAK BISA BERGERAK KE LUAR ZONA" << endl;
+            }
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < 10; j++) {
                     if ((i == y) && (j == x)) {
-                        board[i][j] = 'R';                        
+                        board[i][j] = 'R';
+                    }
+                    else if ((i==y_kecoa) && (j==x_kecoa)){
+                        board[i][j] = 'K';
                     }
                     else {
                         board[i][j] = '.';
@@ -77,7 +146,7 @@ int main() {
                 }
                 cout << "\n";
             }
-  
+
         }
         cout << "1. Bergerak \n" << "3. Keluar \n";
         cout << "Masukkan pilihan: ";
